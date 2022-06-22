e9ccb577241282ac488a03e408521dd025c2ef44
Roman Sommer <roman@resonant-bytes.de>
2022-06-22 22:02:02 +0200

    hi_modules: cleanup and correctly register faust_node skeleton

--- hi_modules/nodes/HiseNodeFactory.cpp
+++ hi_modules/nodes/HiseNodeFactory.cpp
@@ -1719,7 +1719,7 @@ Factory::Factory(DspNetwork* network) :
 #endif
 
 #if HISE_INCLUDE_FAUST
-	// registerNode<faust::faust_node>();
+	registerNodeRaw<faust::faust_node>();
 #endif // HISE_INCLUDE_FAUST
 
 	registerModNode<dp<extra_mod>, data::ui::displaybuffer_editor>();

0787c19005fa119bba34c973ecc3fd9ddeb74bed
Roman Sommer <roman@resonant-bytes.de>
2022-06-28 23:51:32 +0200

    hi_faust: move method definitions out of header

--- hi_faust/FaustNode.h
+++ hi_faust/FaustNode.h
@@ -7,21 +7,14 @@ struct faust_node: public scriptnode::NodeBase {
     SET_HISE_NODE_ID("faust");
     faust_node(DspNetwork* n, ValueTree v) :
 	NodeBase(n, v, 0) { }
-    void initialise(NodeBase* n) { }
-    virtual void prepare(PrepareSpecs specs) override { }
-    virtual void reset() override { }
-    virtual void process(ProcessDataDyn& data) override { }
-    virtual void processFrame(FrameType& data) override { }
-    static NodeBase* createNode(DspNetwork* n, ValueTree v)
-	{
-	    return new faust_node(n, v);
-	}
-    File getFaustRootFile(NodeBase* n)
-	{
-	    auto mc = n->getScriptProcessor()->getMainController_();
-	    auto dspRoot = mc->getCurrentFileHandler().getSubDirectory(FileHandlerBase::DspNetworks);
-	    return dspRoot.getChildFile("CodeLibrary/faust");
-	}
+    // faust_node(DspNetwork* n, ValueTree v);
+    void initialise(NodeBase* n);
+    virtual void prepare(PrepareSpecs specs) override;
+    virtual void reset() override;
+    virtual void process(ProcessDataDyn& data) override;
+    virtual void processFrame(FrameType& data) override;
+    static NodeBase* createNode(DspNetwork* n, ValueTree v);
+    File getFaustRootFile(NodeBase* n);
 };
 }
 }
--- hi_faust/hi_faust.cpp
+++ hi_faust/hi_faust.cpp
@@ -1,3 +1,5 @@
+#include "../JuceLibraryCode/JuceHeader.h"
+
 #if HISE_INCLUDE_FAUST
 #include "FaustNode.cpp"
 #endif // HISE_INCLUDE_FAUST

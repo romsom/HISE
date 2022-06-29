c0c52528616665c47f3c7801757e64c7e37abc2a
Roman Sommer <roman@resonant-bytes.de>
2022-06-29 11:36:28 +0200

    hi_faust: create faust dsp instance from jit factory (broken: name clash)

--- hi_faust/FaustNode.h
+++ hi_faust/FaustNode.h
@@ -1,12 +1,24 @@
 #pragma once
 
+// Faust dsp class
+struct dsp;
+struct llvm_dsp_factory;
+
 namespace scriptnode {
 namespace faust {
+
 
 struct faust_node: public scriptnode::NodeBase {
     SET_HISE_NODE_ID("faust");
     faust_node(DspNetwork* n, ValueTree v) :
-	NodeBase(n, v, 0) { }
+	NodeBase(n, v, 0),
+	sampleRate(0),
+	faustFactory(nullptr),
+	faustDsp(nullptr)
+	{
+	    // dummy code for now:
+	    faustCode = "import(\"stdfaust.lib\"); d(x) = x + X@10000; process = d";
+	}
     // faust_node(DspNetwork* n, ValueTree v);
     void initialise(NodeBase* n);
     virtual void prepare(PrepareSpecs specs) override;
@@ -15,6 +27,15 @@ struct faust_node: public scriptnode::NodeBase {
     virtual void processFrame(FrameType& data) override;
     static NodeBase* createNode(DspNetwork* n, ValueTree v);
     File getFaustRootFile(NodeBase* n);
+
+private:
+    void recompileFaustCode();
+    std::string faustCode;
+    std::string faustErrorMessage;
+    int faustJitOptimize = 0; // -1 is maximum optimization
+    int sampleRate;
+    llvm_dsp_factory* faustFactory;
+    dsp *faustDsp;
 };
 }
 }

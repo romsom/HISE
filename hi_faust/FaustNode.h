f07b5c225c280deab2b001fecbe5123f2180dadd
Roman Sommer <roman@resonant-bytes.de>
2022-07-06 01:44:03 +0200

    hi_faust: add skeleton FaustMenuBar class

--- hi_faust/FaustNode.h
+++ hi_faust/FaustNode.h
@@ -7,6 +7,25 @@ struct faust_wrapper;
 
 struct faust_node: public scriptnode::NodeBase {
     SET_HISE_NODE_ID("faust");
+
+    struct FaustMenuBar : public Component,
+			 public ButtonListener,
+			 public ComboBox::Listener
+
+    {
+
+	FaustMenuBar(faust_node *n);
+	HiseShapeButton addButton;
+	HiseShapeButton editButton;
+	struct Factory : public PathFactory
+	{
+	    Path createPath(const String& p) const override;
+	    String getId() const override { return {}; }
+	} factory;
+
+	WeakReference<faust_node> node;
+    };
+
     faust_node(DspNetwork* n, ValueTree v);
     void initialise(NodeBase* n);
     virtual void prepare(PrepareSpecs specs) override;
@@ -23,6 +42,7 @@ private:
     int _nFramesMax;
     std::vector<float> inputBuffer;
     std::vector<float*> inputChannelPointers;
+    //FaustMenuBar menuBar;
     void resizeBuffer();
     float** getRawInputChannelPointers() {
 	return &inputChannelPointers[0];

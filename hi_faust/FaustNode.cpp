0f520a80ede7e205e8f2adeefce7a05ab2f67473
Roman Sommer <roman@resonant-bytes.de>
2022-08-24 22:59:26 +0200

    faust: remove hard coded dummy faust code

--- hi_faust/FaustNode.cpp
+++ hi_faust/FaustNode.cpp
@@ -21,72 +21,10 @@ faust_node::faust_node(DspNetwork* n, ValueTree v) :
     {
         return new FaustMenuBar(static_cast<faust_node*>(o));
     };
-    // dummy code for now:
-    faust->code =
-        "import(\"math.lib\");\n"
-        "import(\"filter.lib\");\n"
-        "\n"
-        "DELAY_MS_MAX = 25;\n"
-        "SR_MAX = 192000.0;\n"
-        "SR_ = min(SR, SR_MAX);\n"
-        "\n"
-        "ipt = hslider(\"smooth_time\", 0.05, 0, 0.1, 0.001); // s\n"
-        "ip = smooth(tau2pole(ipt));\n"
-        "// : is aggregate, read as signal flow from left to right\n"
-        "// smooth to remove glitches, when moving the slider, then limit, so buffer size can be inferred\n"
-        "d = hslider(\"depth\", 5, 0, DELAY_MS_MAX, 0.01) : ip : min(DELAY_MS_MAX) : max(0); // ms\n"
-        "a = hslider(\"amount\", 0.35, 0, 1, 0.01) : ip : min(1) : max(0); // relative amount\n"
-        "f = hslider(\"feedback\", 0.75, 0, 1, 0.001) : ip; // amount of feedback\n"
-        "s = hslider(\"speed\", 7, 0.1, 25, 0.01) : ip : max(0.1); // hz\n"
-        "l = hslider(\"output level\", 0.5, 0, 1, 0.01) : ip;\n"
-        "\n"
-        "fixed_fdel(n) = \\(x).((1-a) * x@nInt + a * x@(nInt + 1))\n"
-        "with {\n"
-        "     nInt = int(n);\n"
-        "     a = n - nInt;\n"
-        "};\n"
-        "\n"
-        "lfo(wf, p) = p * float(arg) / nSamples : wf\n"
-        "with {\n"
-        "    nSamples = int(SR / s);\n"
-        "    arg = +(1) ~ \\(x).( x * (x % nSamples != 0)) ;\n"
-        "};\n"
-        "\n"
-        "// discard util signal here\n"
-        "c(x) = cal(x) : \\(x,y).(x * l)\n"
-        "with {\n"
-        "     cal(x) = (+(x), +(a * x)) ~ (sd : mux)\n"
-        "     with {\n"
-        "         // single delay stage, customize\n"
-        "     // a instead of d works quite well for simple controls\n"
-        "     // TODO: try to adjust depth according to speed, so depth defines the derivative of lfo\n"
-        "     // note: /s is too much, also prohibits to set s=0\n"
-        "         sd(x,y) = y : fixed_fdel((lfo(sin, 2*PI) + 1) / 2 * a * SR_ / 1000);\n"
-        "     // output signal, feedback signal\n"
-        "             mux = _ <: _, f*_;\n"
-        "    };\n"
-        "};\n"
-        "\n"
-        "process = c, c;\n"
-
-        "";
-    // setup dsp
-    bool success = faust->setup();
-    std::cout << "Faust initialization: " << (success ? "success" : "failed") << std::endl;
-    // TODO: error handling
-    if (!success)
-    {
-        auto p = dynamic_cast<Processor*>(getScriptProcessor());
-
-        debugError(p, "FaustError");
-    }
 
     parameterListener.setCallback(getParameterTree(),
                                   valuetree::AsyncMode::Synchronously, BIND_MEMBER_FUNCTION_2(parameterUpdated));
 
-
-    setupParameters();
-    // we can't init yet, because we don't know the sample rate
     File f = getFaustRootFile();
     // Create directory if it's not already there
     if (!f.isDirectory()) {
@@ -94,6 +32,8 @@ faust_node::faust_node(DspNetwork* n, ValueTree v) :
         //DBG(res);
     }
     DBG(f.getFullPathName());
+    // we can't init yet, because we don't know the sample rate
+    setClass(classId.getValue());
 }
 
 void faust_node::setupParameters()
@@ -290,7 +230,17 @@ void faust_node::loadSource()
     // Load file and recompile
     String code = sourceFile.loadFileAsString();
     faust->code = code.toStdString();
-    faust->setup();
+    // setup dsp
+    bool success = faust->setup();
+    std::cout << "Faust initialization: " << (success ? "success" : "failed") << std::endl;
+    // TODO: error handling
+    if (!success)
+    {
+        auto p = dynamic_cast<Processor*>(getScriptProcessor());
+
+        debugError(p, "FaustError");
+    }
+    setupParameters();
 }
 
 void faust_node::setClass(const String& newClassId)

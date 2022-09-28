1082a2f19c6ea307b92758fbea5e6b8e81319e17
Roman Sommer <roman@resonant-bytes.de>
2022-09-28 19:45:58 +0200

    hi_faust_lib: wrap generateCAuxFiles*() calls (Faust-C-interface)

--- hi_faust_lib/hi_faust_lib.cpp
+++ hi_faust_lib/hi_faust_lib.cpp
@@ -3,4 +3,5 @@
 #endif // HISE_FAUST_USE_LLVM_JIT
 #include "faust_wrap/dsp/interpreter-dsp-c-backend-placeholder.cpp"
 //#include "faust_wrap/dsp/interpreter-dsp.cpp"
-#include "faust_wrap/dsp/libfaust.cpp"
+#include "faust_wrap/dsp/libfaust-c-backend-placeholder.cpp"
+//#include "faust_wrap/dsp/libfaust.cpp"
--- hi_faust_lib/hi_faust_lib_02.cpp
+++ hi_faust_lib/hi_faust_lib_02.cpp
@@ -1,3 +1,6 @@
+// This is the land of the faust C interface, which is needed for windows
+
 #if !HISE_FAUST_USE_LLVM_JIT
 #include "faust_wrap/dsp/interpreter-dsp-c-backend.cpp"
+#include "faust_wrap/dsp/libfaust-c-backend.cpp"
 #endif
